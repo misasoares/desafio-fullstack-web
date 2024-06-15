729da8860c8b77659be70db1e13b545cadaa9a08
misasoares <misasoaresms2@gmail.com>
2024-06-15 11:36:13 -0300

    feature: create validation form

---
+++ src/pages/auth/validations/auth-form.schema.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const defaultValues = {
+  name: "",
+  nickname: "",
+  email: "",
+  password: "",
+  repassword: "",
+};
+
+export const authFormShema = z
+  .object({
+    name: z.string().optional(),
+    nickname: z.string().optional(),
+    email: z
+      .string({ message: "É necessário adicionar um email." })
+      .email({ message: "É necessário adicionar um email." }),
+    password: z.string(),
+  })
+  .superRefine((value, ctx) => {
+    if (value.name && !value.nickname) {
+      ctx.addIssue({
+        path: ["nickname"],
+        message: "É necessário adicionar um nickname.",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+    if (value.name && !value.password) {
+      ctx.addIssue({
+        path: ["password"],
+        message: "É necessário adicionar uma senha.",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+    if (value.email && !value.password) {
+      ctx.addIssue({
+        path: ["password"],
+        message: "É necessário adicionar uma senha.",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+  });
+
+export type TCreateAuthForm = z.infer<typeof authFormShema>;
