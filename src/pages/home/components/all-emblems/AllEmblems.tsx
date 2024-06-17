9decbde24c9941b2349749570ed3509aeee50ae4
misasoares <misasoaresms2@gmail.com>
2024-06-16 23:53:23 -0300

    feature: create emblems filters

--- src/pages/home/components/all-emblems/AllEmblems.tsx
+++ src/pages/home/components/all-emblems/AllEmblems.tsx
@@ -1,22 +1,44 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, TextField, Typography } from "@mui/material";
 import { useAppSelector } from "../../../../store/hooks";
 import { selectEmblems } from "../../store/emblemsSlice";
 import CardEmblem from "./CardEmblem";
+import { useState } from "react";
 
 export default function AllEmblems() {
   const emblems = useAppSelector(selectEmblems).emblems;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredEmblems = emblems.filter(
+    (emblem) =>
+      emblem.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      emblem.slug.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h3">Emblemas</Typography>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3">Emblemas</Typography>
+        </Grid>
       </Grid>
-      {emblems &&
-        emblems.map((emblem) => (
-          <Grid key={emblem.id} item xs={4}>
-            <CardEmblem emblem={emblem} />
-          </Grid>
-        ))}
-    </Grid>
+      <div className="my-4">
+        <TextField
+          label="Pesquisar emblemas"
+          variant="outlined"
+          className=" w-2/5"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
+      <Grid container spacing={2}>
+        {emblems &&
+          filteredEmblems.map((emblem) => (
+            <Grid key={emblem.id} item xs={4}>
+              <CardEmblem emblem={emblem} />
+            </Grid>
+          ))}
+      </Grid>
+    </>
   );
 }
--- src/pages/home/components/my-emblems/MyEmblems.tsx
+++ src/pages/home/components/my-emblems/MyEmblems.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 
 import { useEffect, useState } from "react";
@@ -13,10 +13,17 @@ export default function MyEmblems() {
   const user = useAppSelector(selectUser);
 
   const [haveAllEmblems, setHaveAllEmblems] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   function getEmblemToUser() {
     dispatch(getRandomEmblemToUser(user.id));
   }
+
+  const filteredEmblems = user.emblems.filter(
+    (emblem) =>
+      emblem.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      emblem.slug.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   useEffect(() => {
     if (user.emblems.length === 10) {
@@ -26,12 +33,16 @@ export default function MyEmblems() {
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
+      <Grid container spacing={2} className="items-center">
+        <Grid item xs={4}>
           <Typography variant="h3">Meus Emblemas</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Button onClick={getEmblemToUser} variant="contained">
+          <Button
+            onClick={getEmblemToUser}
+            color={haveAllEmblems ? "error" : "primary"}
+            variant="contained"
+          >
             {haveAllEmblems
               ? "Você já possui todos emblemas"
               : "Resgatar um emblema aleatório"}
@@ -39,9 +50,19 @@ export default function MyEmblems() {
         </Grid>
       </Grid>
 
+      <div className="my-4">
+        <TextField
+          label="Pesquisar emblemas"
+          variant="outlined"
+          className=" w-2/5"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
       <Grid container spacing={2}>
         {user.emblems &&
-          user.emblems
+          filteredEmblems
             .slice()
             .reverse()
             .map((emblem) => (
